b1814ac676eea7d1455e0e67baf534cf199af5fb
Danny393 <danielacosta393@gmail.com>
2020-03-10 20:22:59 -0400

    Start of implementing Post feature

--- app/src/main/java/com/example/instagram_daniel/MainActivity.java
+++ app/src/main/java/com/example/instagram_daniel/MainActivity.java
@@ -3,9 +3,17 @@ package com.example.instagram_daniel;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,6 +29,24 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent logIn = new Intent(MainActivity.this, LogInActivity.class);
                 startActivity(logIn);
+            }
+        });
+    }
+
+    private void queryPosts()
+    {
+        ParseQuery<Post> query = ParseQuery.getQuery(Post.class);
+        query.include(Post.KEY_USER);
+
+        query.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> posts, ParseException e) {
+                if(e == null)
+                {
+                    for(Post post: posts) {
+                        Log.i("Success","Post: " + post.getDescription());
+                    }
+                }
             }
         });
     }
--- app/src/main/java/com/example/instagram_daniel/ParseApp.java
+++ app/src/main/java/com/example/instagram_daniel/ParseApp.java
@@ -3,12 +3,15 @@ package com.example.instagram_daniel;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApp extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
+
+        ParseObject.registerSubclass(Post.class);
 
         final Parse.Configuration config = new Parse.Configuration.Builder(this)
                 .applicationId("myAppId")
